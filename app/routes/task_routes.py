06fa038685bb1608477de6031898f579aece6a39
marjanak <marjana1816@gmail.com>
2024-10-31 22:43:26 -0700

    Done with setup, built task model, created routes with post and get

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,30 @@
-from flask import Blueprint
+from flask import Blueprint,request,Response
+from app.models.task import Task
+from ..db import db
+
+tasks_bp = Blueprint("tasks_bp",__name__, url_prefix="/tasks")
+
+@tasks_bp.post("")
+def create_task():
+    request_body = request.get_json()
+    title = request_body["title"]
+    description = request_body["description"]
+    completed_at = request_body["completed_at"]
+    
+
+    new_task = Task(title=title, description=description, completed_at=completed_at)
+    
+    db.session.add(new_task)
+    db.session.commit()
+
+    response = new_task.to_dict()
+    return response,201
+
+@tasks_bp.get("")
+def get_all_tasks():
+    query = db.select(Task).order_by(Task.id)
+    tasks = db.session.scalars(query)
+
+    tasks_response = [task.to_dict() for task in tasks]
+    return tasks_response
+
